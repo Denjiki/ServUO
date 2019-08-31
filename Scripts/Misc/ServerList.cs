c6e6ad260493db558a3a6f43330ce2bde8230186
Denjiki <denjiki@gmail.com>
2019-08-31 13:16:32 -0400

    Server name & client file location
    Changed server name and client file locations from defaults

--- Scripts/Misc/ServerList.cs
+++ Scripts/Misc/ServerList.cs
@@ -45,7 +45,7 @@ namespace Server.Misc
 
 		public static readonly bool AutoDetect = Config.Get("Server.AutoDetect", true);
 
-		public static string ServerName = Config.Get("Server.Name", "My Shard");
+		public static string ServerName = Config.Get("Apocalypse", "Apocalypse");
 
 		private static IPAddress _PublicAddress;
 
